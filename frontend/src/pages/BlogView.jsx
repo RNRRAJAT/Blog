d7b49b02f2493a4484c385b1de0882c2830f812b
Rajat Roy <rajatroy977@gmail.com>
2025-07-17 23:18:34 +0530

    trying to resolve ._id issue

--- frontend/src/pages/BlogView.jsx
+++ frontend/src/pages/BlogView.jsx
@@ -25,9 +25,9 @@ const BlogView = () => {
     const blogId = params.blogId
     const { blog } = useSelector(store => store.blog)
     const { user } = useSelector(store => store.auth)
-    const selectedBlog = blog.find(blog => blog._id === blogId)
+    const selectedBlog = blog?.find(blog => blog._id === blogId)
     const [blogLike,setBlogLike] = useState(selectedBlog.likes.length)
-    const [liked,setLiked]= useState(selectedBlog.likes.includes(user._id) || false)
+    const [liked,setLiked]= useState(selectedBlog?.likes.includes(user._id) || false)
     const dispatch = useDispatch()
 
     const changeTimeFormat = (isDate)=>{

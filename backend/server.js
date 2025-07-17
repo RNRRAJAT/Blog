199b08acbe8f43490d2860f16e5fa2b22c92882e
Rajat Roy <rajatroy977@gmail.com>
2025-07-17 23:44:34 +0530

    refresh issue

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cors({
     credentials:true
 }))
 
-const _dirname = path.resolve()
+const __dirname = path.resolve()
 
 const PORT = process.env.PORT || 3000;
 
@@ -28,10 +28,18 @@ app.use("/api/v1/user",userRoute);
 app.use("/api/v1/blog",blogRoute);
 app.use("/api/v1/comment",commentRoute);
 
-app.use(express.static(path.join(_dirname,"/frontend/dist")))
+// app.use(express.static(path.join(_dirname,"/frontend/dist")))
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
 // app.get("*",(_,res)=>{
 //     res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
 // })
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 app.listen(PORT,()=>{
     connectDB();

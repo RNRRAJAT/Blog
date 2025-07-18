6adaefa5ce8ae09fc2c8e3d07af8312c53aab0f8
Rajat Roy <rajatroy977@gmail.com>
2025-07-18 12:05:22 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://blog-cyan-eta-16.vercel.app/",
+    origin:"https://blog-cyan-eta-16.vercel.app",
     credentials:true
 }))
 
